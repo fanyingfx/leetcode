8984c694b997a411a7498411ada1da52986b9f83
fanyingfx <fanzhenxin@outlook.com>
2022-04-24 23:27:14 +0800

    update

---
+++ 6_Z 字形变换.py
@@ -0,0 +1,10 @@
+# @algorithm @lc id=6 lang=python3 
+# @title zigzag-conversion
+
+
+from cn.Python3.mod.preImport import *
+# @test("PAYPALISHIRING",3)="PAHNAPLSIIGYIR"
+# @test("PAYPALISHIRING",4)="PINALSIGYAHRPI"
+# @test("A",1)="A"
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
---
+++ cn/Python3/9_回文数.py
@@ -0,0 +1,10 @@
+# @algorithm @lc id=9 lang=python3 
+# @title palindrome-number
+
+
+from cn.Python3.mod.preImport import *
+# @test(121)=true
+# @test(-121)=false
+# @test(10)=false
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
